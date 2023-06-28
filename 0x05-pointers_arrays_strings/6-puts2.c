841e677da736193c7e3dd94748d3b7069032ce29
Mohammad-syria <mohammadjamos2003@gmail.com>
2023-06-27 18:10:21 -0700

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - prints every other char
+ * of a string
+ * @str: input
+ *
+*/
+
+void puts2(char *str)
+{
+int n = 0;
+
+for (; str[n] != '\0'; n++)
+{
+	if (n % 2 == 0)
+		_putchar(str[n]);
+}
+_putchar('\n');
+}

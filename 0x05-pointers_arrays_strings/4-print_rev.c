5aca91cded6b7b6352673df7cc1498fba1b37691
Mohammad-syria <mohammadjamos2003@gmail.com>
2023-06-27 17:35:15 -0700

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in rev
+ * @s: input
+ *
+*/
+
+void print_rev(char *s)
+{
+int l = 0;
+
+while (s[l])
+{
+	l++;
+}
+l--;
+while (l >= 0)
+{
+	_putchar(s[l]);
+	l--;
+}
+_putchar('\n');
+}
